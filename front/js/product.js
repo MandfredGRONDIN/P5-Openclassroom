7cd0038813a15f8982d8f1e6051193273d9eb1f7
Mandfred GRONDIN <mandfred974@hotmail.fr>
2022-08-20 13:02:48 +0200

    Update product.js
    Rajout des couleurs des produits

--- front/js/product.js
+++ front/js/product.js
@@ -8,14 +8,22 @@ fetch(`http://localhost:3000/api/products/` + product_id)
     displayProductInfos(product);
   });
 
-// Afficher les informations du produit
 let product_img = document.querySelector(".item__img");
 let product_title = document.querySelector("#title");
 let product_price = document.querySelector("#price");
 let product_description = document.querySelector("#description");
+let product_colors = document.querySelector("#colors");
+let product_nb = document.querySelector("itemQuantity");
+
+// Afficher les informations du produit
 function displayProductInfos(product) {
   product_img.innerHTML += `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
   product_title.innerHTML += `<h1 id="title"> ${product.name} </h1>`;
   product_price.innerHTML += `<span id="price"> ${product.price} </span>`;
   product_description.innerHTML += `<p id="description"> ${product.description} </p>`;
+  let product_color = product.colors;
+  for (let i = 0; i < product_color.length; i++){
+    product_colors.innerHTML += `<option value="${product_color[i]}">${product_color[i]}</option>`
+    console.log(product_color[i])
+  }
 }
