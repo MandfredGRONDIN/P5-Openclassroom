42559553500c0c709e0a40b4d1da583f6418e758
Mandfred GRONDIN <mandfred974@hotmail.fr>
2022-08-30 00:26:38 +0200

    Update cart
    Affiche du tableau récapitulatif des produits

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,32 +1,35 @@
 let cart = JSON.parse(localStorage.getItem("product_client"));
 
 getAPIProducts(cart)
-
-
+// Récupérer les infos manquantes des produits
 async function getAPIProducts(product_client){
   try{
     for(let i = 0; i < product_client.length; i++){
       await fetch(`http://localhost:3000/api/products/` + cart[i].id)
       .then((res) => res.json())
       .then((data) => (productData = data))
-      test(productData);
-    }console.log(cart)
+       if (cart[i].id == productData._id){
+        productData.colors = cart[i].color;
+        productData.quantity = cart[i].quantity;
+       }
+      createProduct(productData);
+    }
   } catch(err){
     console.error(err);
   }
 }
 
-
- function test(productData){
+// Création des produits et leurs infos
+function createProduct(productData){
   let cart_items = document.querySelector("#cart__items");
         cart_items.innerHTML += `<article class="cart__item" data-id="${productData._id}" data-color="${productData.color}">
         <div class="cart__item__img">
-          <img src="${productData.imageUrl}" alt="${productData.colors}">
+          <img src="${productData.imageUrl}" alt="${productData.altTxt}">
         </div>
         <div class="cart__item__content">
           <div class="cart__item__content__description">
             <h2>${productData.name}</h2>
-            <p>${productData.color}</p>
+            <p>${productData.colors}</p>
             <p>${productData.price} €</p>
           </div>
           <div class="cart__item__content__settings">
